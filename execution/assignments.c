cb108efe5f8c3f55fde6dc992606958ae6816785
tmarts <tmarts@student.42heilbronn.de>
2023-07-21 18:39:56 +0200

    add to var_list with str function

--- execution/assignments.c
+++ execution/assignments.c
@@ -6,7 +6,7 @@
 /*   By: tmarts <tmarts@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 17:37:42 by tmarts            #+#    #+#             */
-/*   Updated: 2023/07/21 16:36:43 by tmarts           ###   ########.fr       */
+/*   Updated: 2023/07/21 18:36:21 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,37 @@ static int	full_assign(t_var_list *new, char *word, char *delim_pt)
 }
 
 t_exec_exit_code	add_to_var_list(t_var_list *var_list, \
+													char *str, int flag)
+{
+	t_var_list		*new_node;
+	char			*delim_pt;
+
+	new_node = (t_var_list *)ft_calloc(1, sizeof(t_var_list));
+	if (!new_node)
+		return (EXEC_MALLOC_ERROR);
+	new_node->env_flag = flag;
+	delim_pt = ft_strchr(str, '=');
+	if (delim_pt == NULL)
+	{
+		new_node->key = ft_strdup(str);
+		if (!new_node->key)
+			return (free(new_node), EXEC_MALLOC_ERROR);
+	}
+	else if (full_assign(new_node, str, delim_pt) != 0)
+		return (EXEC_MALLOC_ERROR);
+	ft_lstadd_back_ms(&var_list, new_node);
+}
+
+t_exec_exit_code	add_assignments(t_var_list *var_list, \
 									t_assignment *assign, int flag)
 {
 	t_assignment	*cur_assign;
-	t_var_list		*new_node;
-	char			*delim_pt;
 
 	cur_assign = assign;
 	while (cur_assign != NULL)
 	{
-		new_node = (t_var_list *)ft_calloc(1, sizeof(t_var_list));
-		if (!new_node)
-			return (EXEC_MALLOC_ERROR);
-		new_node->env_flag = flag;
-		delim_pt = ft_strchr(cur_assign->word, '=');
-		if (delim_pt == NULL)
-		{
-			new_node->key = ft_strdup(cur_assign->word);
-			if (!new_node->key)
-				return (free(new_node), EXEC_MALLOC_ERROR);
-		}
-		else if (full_assign(new_node, cur_assign->word, delim_pt) != 0)
-			return (EXEC_MALLOC_ERROR);
-		ft_lstadd_back_ms(&var_list, new_node);
+		if (add_to_var_list(var_list, cur_assign->word, flag))
+			return (EXEC_FAIL);
 		cur_assign = cur_assign->next;
 	}
 	return (EXEC_SUCCESS);
